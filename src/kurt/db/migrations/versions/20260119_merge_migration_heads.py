74e589516081fc5ba39268423d686cbb97cdbf93
huraultj <julien.hurault@sumeo.io>
2026-01-19 12:18:14 +0100

    fix: Apply ruff linting to merge migration

--- src/kurt/db/migrations/versions/20260119_merge_migration_heads.py
+++ src/kurt/db/migrations/versions/20260119_merge_migration_heads.py
@@ -8,10 +8,6 @@ Create Date: 2026-01-19 11:13:28.015585+00:00
 
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
 revision: str = "0b94d9578a11"
 down_revision: Union[str, None] = ("84688a81b9ab", "008_rls_workspace")
