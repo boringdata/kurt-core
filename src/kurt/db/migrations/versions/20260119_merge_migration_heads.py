9e694e57b4499c0e9be5df4e250bba1e7eb8397f
huraultj <julien.hurault@sumeo.io>
2026-01-19 12:16:49 +0100

    fix: Merge migration heads (84688a81b9ab + 008_rls_workspace)

---
+++ src/kurt/db/migrations/versions/20260119_merge_migration_heads.py
@@ -0,0 +1,27 @@
+"""merge_migration_heads
+
+Revision ID: 0b94d9578a11
+Revises: 84688a81b9ab, 008_rls_workspace
+Create Date: 2026-01-19 11:13:28.015585+00:00
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "0b94d9578a11"
+down_revision: Union[str, None] = ("84688a81b9ab", "008_rls_workspace")
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
